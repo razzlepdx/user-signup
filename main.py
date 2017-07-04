6c4fdf84b14c3e3ec8bc6c36a9c22626f85fe7bf
razzlepdx <rwilson.or@gmail.com>
2017-07-04 13:14:28 -0700

    added partial email validation function

--- main.py
+++ main.py
@@ -1,36 +1,65 @@
 from flask import Flask, request, redirect, render_template
 import cgi
 
+# TODOS:
+#       -add regex validation (optional)
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+def validate_input(input):
+    ''' validates all user inputs and returns True/False based on requirements met '''
+    if len(input) < 3 or len(input) > 20:
+        return False
+    for c in input:
+        if c == " ":
+            return False
+    return True
+
 def check_user(username):
-    if len(username) < 3 or len(username) > 20:
+    ''' returns username/error values after validation '''
+    if validate_input(username) == False:
         username_error = "That's not a valid username, please enter a username between 3-20 characters."
         username = ""
     else:
         username_error = ''
     return username, username_error
 
-def check_pass1(pass1, pass2):
-    if len(pass1) < 3 or len(pass1) > 20:
+def check_pass1(pass1):
+    ''' returns initial password error message after validation '''
+    if validate_input(pass1) == False:
         password1_error = "That's not a valid password, password must be between 3-20 characters."
-        password1 = ''
-        password2 = ''
     else: 
         password1_error = ''
-    return password1, password1_error
+    return password1_error
 
 def check_pass2(pass2, pass1):
+    ''' validates second password field and returns second password error message '''
     if pass2 != pass1:
         password2_error = "Passwords don't match, please try again."
-        password2 = ''
-        password1 = ''
     else:
         password2_error = ''
-    return password2, password2_error 
+    return password2_error 
 
-# def check_email
+def check_email(email):
+    ''' validates non-empty email fields and returns email/error message values '''
+    req_values = 0
+
+    if len(email) == 0:
+        email_error = ''
+    
+    for c in email:
+        if c == '@' or c == '.':
+            req_values += 1
+
+    if validate_input(email) == False or req_values != 2:
+        email = ''
+        email_error = "Please enter a valid email (between 3-20 characters, no spaces, contains @ and .)"
+    
+    if req_values == 2:
+        email_error = ''   
+    
+    return email, email_error
 
 @app.route("/", methods=["GET","POST"])
 def signup():
@@ -41,13 +70,10 @@ def signup():
     if request.method == "POST":
         ''' validates all user inputs on signup screen '''
         username, username_error = check_user(username = request.form['username'])
-        password1, password1_error = check_pass1(pass1 = request.form['pass1'], pass2 = request.form['pass2'])
-        password2, password2_error = check_pass2(pass2 = request.form['pass2'], pass1 = password1)
-        # email, email_error = check_email(email = request.form('email'))
-        email = request.form['email']
-        email_error = ''
-        error_messages = username_error + password1_error + password2_error 
-        # + email_error
+        password1_error = check_pass1(pass1 = request.form['pass1'])
+        password2_error = check_pass2(pass2 = request.form['pass2'], pass1 = request.form['pass1'])
+        email, email_error = check_email(email = request.form['email'])
+        error_messages = username_error + password1_error + password2_error + email_error
         
         if len(error_messages) > 0:
             return render_template('signup.html', 
@@ -57,11 +83,4 @@ def signup():
         else:
             return render_template('welcome.html', username = username)
 
-# @app.route('/welcome', methods=["GET",'POST'])
-# def welcome_user():
-#     username = request.form['username']
-#     return render_template('welcome.html', name = username)
-
 app.run()
-
-# pass1 = password1,
