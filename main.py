9dbd237ad515de7a3752b212aa2a01ad6c073d54
razzlepdx <rwilson.or@gmail.com>
2017-07-05 11:51:41 -0700

    completed requirements

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 import cgi
-
-# TODOS:
-#       -add regex validation (optional)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -43,32 +40,24 @@ def check_pass2(pass2, pass1):
 
 def check_email(email):
     ''' validates non-empty email fields and returns email/error message values '''
-    req_values = 0
-
     if len(email) == 0:
         email_error = ''
-    
-    for c in email:
-        if c == '@' or c == '.':
-            req_values += 1
 
-    if validate_input(email) == False or req_values != 2:
+    elif validate_input(email) == False or email.find('@') == -1 or email.find('.') == -1:
         email = ''
         email_error = "Please enter a valid email (between 3-20 characters, no spaces, contains @ and .)"
-    
-    if req_values == 2:
+    else:
         email_error = ''   
-    
     return email, email_error
 
 @app.route("/", methods=["GET","POST"])
 def signup():
     if request.method == "GET":
-        ''' displays initial signup screen '''
+        # displays initial signup screen
         return render_template('signup.html')
     
     if request.method == "POST":
-        ''' validates all user inputs on signup screen '''
+        # validates all user inputs on signup screen
         username, username_error = check_user(username = request.form['username'])
         password1_error = check_pass1(pass1 = request.form['pass1'])
         password2_error = check_pass2(pass2 = request.form['pass2'], pass1 = request.form['pass1'])
@@ -76,11 +65,13 @@ def signup():
         error_messages = username_error + password1_error + password2_error + email_error
         
         if len(error_messages) > 0:
+            # displays error messages and clears input fields as needed
             return render_template('signup.html', 
             username = username, username_error = username_error, 
-            pass1_error = password1_error, pass2_error = password2_error,
+            pass1_error = password1_error, 
+            pass2_error = password2_error,
             email = email, email_error = email_error)
         else:
+            # if no errors, displays welcome screen
             return render_template('welcome.html', username = username)
-
 app.run()
